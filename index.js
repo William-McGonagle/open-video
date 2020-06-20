df042d562055a38b8fc77b8a8644b3c77b833bb0
knuf <knuf@gofuckyourself.com>
2020-06-20 14:53:01 -0400

    Added Auth System

---
+++ index.js
@@ -0,0 +1,203 @@
+require('dotenv').config();
+
+/////////////
+// IMPORTS //
+/////////////
+
+const Sequelize = require('sequelize');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+
+//////////////
+// VAR DEFS //
+//////////////
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'sqlite',
+  host: 'localhost',
+  storage: 'database.sqlite'
+});
+const app = express();
+const port = 8000 || process.env.PORT;
+
+///////////////////////
+// MODEL DEFINITIONS //
+///////////////////////
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.TEXT,
+  email: Sequelize.TEXT
+});
+
+var Video = sequelize.define('video', {
+  title: Sequelize.STRING,
+  description: Sequelize.TEXT
+});
+
+User.hasMany(Video); // Video Owners
+
+sequelize.sync();
+
+////////////////
+// MIDDLEWARE //
+////////////////
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+///////////////
+// FUNCTIONS //
+///////////////
+
+function checkPassword(password) {
+
+    // this function is used to require passwords to fill characteristics
+
+    return false; // means everything is good
+
+}
+
+function checkEmail(email) {
+
+  // this function is used to require emails to fill characteristics
+
+  return false; // means everything is good
+
+}
+
+function checkUsername(username) {
+
+  // this function is used to require usernames to fill characteristics
+
+  return false; // means everything is good
+
+}
+
+////////////
+// ROUTES //
+////////////
+
+app.post('/api/v1/user/create', function (req, res) {
+
+  // check user filled out entire form
+  if (req.body.username == undefined || req.body.email == undefined || req.body.password == undefined) return res.sendStatus(400);
+
+  // check all data fits the molds
+  if (checkUsername(req.body.username) || checkEmail(req.body.email) || checkPassword(req.body.password)) return res.sendStatus(400);
+
+  // check data doesn't already exist
+  User.findAll({
+    where: {
+      [Sequelize.Op.or]: [
+        {
+          username: req.body.username
+        },
+        {
+          email: req.body.email
+        }
+      ]
+    }
+  }).then(function (data) {
+
+    if (data.length > 0) return res.sendStatus(409);
+
+    // hash password
+    bcrypt.hash(req.body.password, 10, function(err, hash) {
+
+      if (err) {
+
+        console.log(err);
+        return res.sendStatus(500);
+
+      }
+
+      // enter data into database
+      User.create({
+        username: req.body.username,
+        password: hash,
+        email: req.body.email
+      }).then(function (insertData) {
+
+        // create and send jwt
+        return res.json({
+          token: jwt.sign({
+            id: insertData.id,
+            username: insertData.username,
+            email: insertData.email
+          }, process.env.privateKey)
+        });
+
+      }).error(function (error) {
+
+        console.log(error);
+        return res.sendStatus(500);
+
+      });
+
+    });
+
+  }).error(function (error) {
+
+    console.log(error);
+    return res.sendStatus(500);
+
+  });
+
+});
+
+app.post('/api/v1/user/login', function (req, res) {
+
+  // check user filled out entire form
+  if (req.body.email == undefined || req.body.password == undefined) return res.sendStatus(400);
+
+  // find user
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(function (data) {
+
+    if (data == null) return res.sendStatus(404);
+
+    // check password hash
+    bcrypt.compare(req.body.password, data.password, function(err, result) {
+
+      if (err) {
+
+        console.log(err);
+        return res.sendStatus(500);
+
+      }
+
+      if (!result) return res.sendStatus(401);
+
+      // create and send jwt
+      return res.json({
+        token: jwt.sign({
+          id: data.id,
+          username: data.username,
+          email: data.email
+        }, process.env.privateKey)
+      });
+
+    });
+
+  }).error(function (error) {
+
+    console.log(error);
+    return res.sendStatus(500);
+
+  });
+
+});
+
+//////////////
+// END CODE //
+//////////////
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
